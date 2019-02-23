d4762d98affe428d16b36d66067c08ac57fec017
joao <joao182cx@gmail.com>
2019-02-23 15:31:14 -0300

    Criação do array de Strings que aramzena algumas palavras que estão ortograficamente corretas. As palavras são carro, computador, impressora, monitor, teclado, impressora, jogador, futebol.

--- src/CorretorOrtografico.java
+++ src/CorretorOrtografico.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
 
 public class CorretorOrtografico {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String palavra;
+		String[] palavrasCorretas = {"carro", "computador", "impressora", "monitor", "teclado", "impressora", "jogador", "futebol"};
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Escreva um palavra:");
+		palavra = input.nextLine();
 
 	}
 
