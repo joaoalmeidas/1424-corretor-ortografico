9ef6d135f92860b552681d77ad11e94efb5c4ea4
joao <joao182cx@gmail.com>
2019-02-23 15:41:14 -0300

    Criação da verificação que determina se a palavra foi corretamente escrita.

--- src/CorretorOrtografico.java
+++ src/CorretorOrtografico.java
@@ -11,7 +11,33 @@ public class CorretorOrtografico {
 		
 		System.out.println("Escreva um palavra:");
 		palavra = input.nextLine();
+		
+		if(verificaOrtografia(palavra, palavrasCorretas) == true) {
+			
+			System.out.println("A palavra foi escrita corretamente.");
+			
+		}else {
+			
+			System.out.println("A palavra não foi escrita corretamente.");
+			
+		}
 
+	}
+	
+	public static boolean verificaOrtografia(String palavra, String[] palavrasCorretas) {
+		
+		for(int i = 0; i < palavrasCorretas.length; i++) {
+			
+			if(palavra.equals(palavrasCorretas[i])) {
+				
+				return true;
+				
+			}
+			
+		}
+		
+		return false;
+			
 	}
 
 }
